8b61eaedd5910961e896bbae6abcc0a11c7de14a
Srujan Gopu <srujanjack@gmail.com>
2020-03-29 12:49:39 -0500

    Added root html and root component

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content={
+              'user-scalable=0, initial-scale=1, ' +
+              'minimum-scale=1, width=device-width, height=device-height'
+            }
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}

5b9dc1fde84ba9053ab6f37c2d5c2ed39aeee46a
Srujan Gopu <srujanjack@gmail.com>
2020-04-18 14:53:56 -0500

    added client server

---
+++ pages/[...all].js
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+class All extends React.Component {
+
+    constructor(props){
+        super(props);
+        console.log(props)
+    }
+
+    render() {
+        return (<div>
+            this is all page
+        </div>);
+    }
+}
+
+export default All;
